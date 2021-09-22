cb05edad02c0f727cbb317df2cfe877b5f971f3e
Nguyễn Hữu Tường <64581048+tuong1403@users.noreply.github.com>
2021-09-22 16:40:42 +0700

    Tuong: add react-toastify to show notification

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,31 @@
-import './App.css';
+import "./App.css";
+import { ToastContainer } from "react-toastify";
+import notification from "components/Notification";
 
 function App() {
+  const handleSuccess = () => {
+    notification("ok", "success");
+  };
+  const handleError = () => {
+    notification("ok", "error");
+  };
+
   return (
-    <div className="App">
-      
+    <div>
+      <button onClick={handleSuccess}>Success</button>
+      <button onClick={handleError}>Error</button>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        theme="colored"
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </div>
   );
 }
