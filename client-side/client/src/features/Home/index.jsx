8b8006f4955de82799b865099df64684381a20c0
Nguyễn Hữu Tường <64581048+tuong1403@users.noreply.github.com>
2021-09-29 10:41:13 +0700

    [Tuong]: create two project for admin and system manager

--- client-side/client/src/features/Home/index.jsx
+++ client-side/client/src/features/Home/index.jsx
@@ -2,10 +2,12 @@ import Footer from "components/Footer";
 import Header from "components/Header";
 import { Fragment } from "react";
 import { Switch, Route, useRouteMatch } from "react-router-dom";
-import HomeGuest from "./pages/HomeGuest";
-import SignInGuest from "./pages/SignInGuest";
-import SignUpGuest from "./pages/SignUpGuest";
 import NotFoundPage from "../../components/404";
+import { lazy } from "react";
+
+const SignInGuest = lazy(() => import("./pages/SignInGuest"));
+const SignUpGuest = lazy(() => import("./pages/SignUpGuest"));
+const HomeGuest = lazy(() => import("./pages/HomeGuest"));
 
 const HomePage = () => {
   const { url } = useRouteMatch();
