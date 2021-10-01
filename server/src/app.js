c452c40c942013880578a67e395be63e68a6ac13
Đặng Nguyễn Hoài Thư <dangnguyenhoaithu08012000@gmail.com>
2021-10-01 19:23:36 +0700

    implement cors

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
+const cors = require('cors');
 
 const route = require('./routes/index');
 
+//Implement cors
+app.use(cors());
+//Access-Controll-Allow-Origin
+app.options('*', cors());
 //MIDDLEWWARE
 console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
