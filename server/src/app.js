f7d1e08795c155cf48d7b8de671d587abbc5c9fc
unknown <dangnguyenhoaithu08012000@gmail.com>
2021-11-04 15:19:16 +0700

    disable rate limit

--- server/src/app.js
+++ server/src/app.js
@@ -25,12 +25,12 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 //limit request from same API
-const limiter = rateLimit({
-  max: 100,
-  windowMs: 60 * 60 * 1000,
-  message: 'Too many requests from this IP, please try again in an hour',
-});
-app.use('/api', limiter);
+// const limiter = rateLimit({
+//   max: 100,
+//   windowMs: 60 * 60 * 1000,
+//   message: 'Too many requests from this IP, please try again in an hour',
+// });
+// app.use('/api', limiter);
 
 //Body-parse, reading data from body into req.body
 app.use(express.json());
